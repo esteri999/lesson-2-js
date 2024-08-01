aa5dec319142a3b0eafef1af0c130d662f1b05e7
EsteriMikaberidze <154770025+esteri999@users.noreply.github.com>
2024-08-01 18:37:11 +0400

    js

---
+++ script.js
@@ -0,0 +1,21 @@
+//'use strict';
+
+let array1 = [1,2,4,-1,-3,8,7,0,-5,18,12];
+ 
+for( let x = 0;  x < array1.length; x++) {
+if (array1[x] > 0 && array1[x]  < 10){
+    console.log(array1[x]); } 
+}
+
+
+
+let a=100;
+
+for ( a=5; a <= 100; a++) {
+    console.log(a);
+}
+
+let array2= [1, 2, 3, 4, 5];
+
+
+//mas davamtavreb gpirdebit)))
